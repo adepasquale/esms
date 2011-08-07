9664a49b48d48f38adc02d614591d2354a8e376d
andrea@de-pasquale.name <andrea@de-pasquale.name@631e6731-3811-ef02-6a52-7d06675a2e5b>
2011-08-07 17:54:29 +0000

    Draft of telephony provider.

---
+++ android/src/com/googlecode/awsms/provider/Telephony.java
@@ -0,0 +1,53 @@
+package com.googlecode.awsms.provider;
+
+import java.util.List;
+
+import com.googlecode.esms.account.Account;
+import com.googlecode.esms.message.SMS;
+
+/**
+ * Default account to use SIM messaging service. 
+ * @author Andrea De Pasquale
+ */
+public class Telephony extends Account {
+  private static final long serialVersionUID = 1L;
+  
+  public Telephony() {
+    super(null);
+  }
+  
+  @Override
+  public int calcRemaining(int length) {
+    // TODO use same method of default app
+    return 0;
+  }
+
+  @Override
+  public int calcFragments(int length) {
+    // TODO use same method of default app
+    return 0;
+  }
+
+  @Override
+  public List<String> getSenderList() {
+    // TODO retrieve SIM phone number
+    return null;
+  }
+
+  @Override
+  public Result login() {
+    return Result.SUCCESSFUL;
+  }
+
+  @Override
+  public Result logout() {
+    return Result.SUCCESSFUL;
+  }
+
+  @Override
+  public Result send(SMS sms) {
+    // TODO use telephony provider
+    return Result.UNSUPPORTED_ERROR;
+  }
+
+}
